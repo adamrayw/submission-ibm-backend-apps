3317243791a8fa9c185ac6c33fd41389717ff399
adamrayw <raywibowo68@gmail.com>
2023-04-01 12:55:31 +0700

    install jsonwebtoken

--- task-3.js
+++ task-3.js
@@ -7,8 +7,8 @@ const books = [
     { id: 3, title: '1984', author: 'George Orwell', isbn: '978-3-16-148430-0' },
     { id: 4, title: 'Pride and Prejudice', author: 'Jane Austen', isbn: '978-3-16-148440-0' },
     { id: 5, title: 'The Catcher in the Rye', author: 'J.D. Salinger', isbn: '978-3-16-148450-0' },
-    { id: 6, title: 'The Picture of Dorian Gray', author: 'Oscar Wilde', isbn: '978-3-16-148460-0' },
-    { id: 7, title: 'The Importance of Being Earnest', author: 'Oscar Wilde', isbn: '978-3-16-148470-0' },
+    { id: 6, title: 'The Picture of Dorian Gray', author: 'Oscar WilOscar Wildede', isbn: '978-3-16-148460-0' },
+    { id: 7, title: 'The Importance of Being Earnest', author: '', isbn: '978-3-16-148470-0' },
 ];
 
 app.get('/books/:author', (req, res) => {
--- task-7.js
+++ task-7.js
@@ -1,18 +1,61 @@
 const express = require('express');
 const router = express.Router();
-const User = require('./userModel');
+const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser');
+const app = express();
 
-router.post('/login', async (req, res) => {
-    const { email, password } = req.body;
-
-    try {
-        const user = await User.findOne({ email });
-
-        if (!user) {
-            return res.status(401).json({ message: 'Invalid email or password' })
-        }
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Something went wrong' });
+const users = [
+    {
+        username: 'john',
+        password: 'password123',
+        email: 'john@example.com'
+    },
+    {
+        username: 'jane',
+        password: 'password456',
+        email: 'jane@example.com'
     }
+];
+
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    const user = users.find(u => u.username === username && u.password === password);
+
+    if (user) {
+        const token = jwt.sign({ user }, 'secret');
+
+        res.json({ message: 'Login successful', token });
+    } else {
+        res.status(401).json({ message: 'Invalid username or password' });
+    }
+});
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (token == null) {
+        return res.sendStatus(401);
+    }
+
+    jwt.verify(token, 'secret', (err, user) => {
+        if (err) {
+            return res.sendStatus(403);
+        }
+
+        req.user = user;
+        next();
+    });
+}
+
+
+// start the server
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
 });
